4979ac67378837f5189d62a008fff882af545fa2
Asphyxia <dario.cavuotti@gmail.com>
2013-05-19 06:30:39 -0300

    Fixed cache-key rewrite method

--- core/cache/Cache.php
+++ core/cache/Cache.php
@@ -26,11 +26,13 @@ class Cache {
      * @var type 
      */
     private $handler;
+    
     /**
      *
      * @var type 
      */
     private $cache;
+
     /**
      *
      * @var type 
@@ -42,6 +44,7 @@ class Cache {
      * @var type 
      */
     public static $INVALID_CACHE_KEY = -1;
+
     /**
      *
      * @var type 
@@ -57,42 +60,55 @@ class Cache {
         if (!isset($this->config['cachePath'])) {
             throw new Exception('Cache path not defined!');
         }
+        // Logger::warn('Config: ' . print_r($this->config,1));
         $this->handler = new StashFS(array(
             'path' => realpath($this->config['cachePath']),
         ));
         StashBox::setHandler($this->handler);
     }
-    
+
+    /**
+     *
+     * @param string $dataKey cache-key to be rewrite
+     */
+    private function getDataKey($dataKey) {
+        foreach ($this->config['key'] as $arr) {
+            $arr['name'] = $quoted = preg_quote($arr['name'], '/');
+            // Logger::info(print_r($arr, 1));
+
+            $countReplacements = 0;
+            $res = preg_replace('/' . $quoted . '/', $arr['value'], $dataKey, -1, $countReplacements);
+            if (null !== $res && $countReplacements >= 1) {
+                return $res;
+            }
+        } 
+        return null;
+    }
+
     /**
      * 
      * @param type $dataKey
      * @return boolean 
      */
     public function rebuild($dataKey) {
-        Logger::log($dataKey);
-        Logger::log($this->config);
-        
-        foreach ($this->config as $url => $key) {
-            $quoted = preg_quote($url, '/');
-            if (null !== $res = preg_replace('/' . $quoted . '/', $key, $dataKey)) {
-                $dataKey = $res;
-            }
-        } 
-        $this->dataKey = trim($dataKey);
-        
+        $rewriteKey = $this->getDataKey($dataKey);
+        $this->dataKey = !empty($rewriteKey) ? $rewriteKey : $dataKey;
+
         if (!empty($this->dataKey)) {
             Logger::warn('Cache-Key: ' . $this->dataKey);
+            
             $this->cache = StashBox::getCache($this->dataKey);
             $this->data = $this->cache->get();
-            if ($this->cache->isMiss() === true || !empty($_POST) || isset($_REQUEST['rape'])) {
-                return true;
-            }
+
+            return $this->cache->isMiss();
+
         }else{
-            Logger::error('Invalid Cache-key!');
+            Logger::error('Invalid Cache-key!: ' . $this->dataKey);
             return self::$INVALID_CACHE_KEY;
         }
         return false;
     }
+
     /**
      *
      * @return type 
@@ -100,6 +116,7 @@ class Cache {
     public function invalidateCache() {
         Logger::warn('Rebuild-Cache: True');
         Logger::warn('Cache-Key: ' . $this->dataKey);
+
         if (!empty($this->dataKey)) {
             return $this->cache->clear();
         }else{
@@ -115,7 +132,7 @@ class Cache {
      */
     public function setData($data) {
         if (false === $this->cache->set($data)) {
-            Logger::log('Cache-error: Couln\'t save cache!');
+            Logger::log('Cache-error: Couldn\'t save cache!');
             return self::$ERROR_WRITTING_CACHE;
         }
         return true;
@@ -132,6 +149,7 @@ class Cache {
             return false;
         }
     }
+
     /**
      * 
      */
@@ -139,6 +157,7 @@ class Cache {
         @ob_clean();
         ob_start();
     }
+
     /**
      *
      * @return type 
